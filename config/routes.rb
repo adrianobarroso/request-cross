310b99b921c09e081c8882d99897b4ba44168f24
Adriano Barroso <wiermann@broou.com>
2016-09-24 12:16:39 -0300

    add user profile update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'profile', to: 'profile#index'
+  patch 'profile', to: 'profile#update'
+
   devise_for :users
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
